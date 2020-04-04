42551ee2afe0ed018f3cc7aa4253e97d014e1b4d
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-04 11:23:15 +0200

    Add initial files.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    // read images from the given path and return a list
+    // for every image in images
+    // create a copy of the original image
+    // convert image into RGB
+    // apply Gaussian Blur on image
+    // apply canny on image
+    // apply Hough transform
+    // apply Hough line detection, get line candidates
+    // get final lines from the candidates
+    // filter the slope
+    // filter region of interest
+    // further filtering
+    // create a copy of the original image
+    // visualize the final lines on the copy image
+    // save the visualized image in the output path
+    return 0;
+}
