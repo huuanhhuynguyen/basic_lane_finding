7d31490933747ec324b8445d76850ed3bbfa455a
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-04 14:54:25 +0200

    Display an image.

---
+++ include/display.h
@@ -0,0 +1,14 @@
+#ifndef SELF_01_LANE_FINDING_BASIC_CPP_DISPLAY_H
+#define SELF_01_LANE_FINDING_BASIC_CPP_DISPLAY_H
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+void display(const cv::Mat& image)
+{
+    namedWindow( "", cv::WINDOW_AUTOSIZE );
+    imshow( "", image );
+    cv::waitKey(0);
+}
+
+#endif //SELF_01_LANE_FINDING_BASIC_CPP_DISPLAY_H
