7953c73f58fd5122d3256ccab82fd30bb9f5f93b
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-04 15:35:45 +0200

    Apply Hough line detection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include "opencv2/imgproc.hpp"
+
 #include "read.h"
+#include "display.h"
 
 int main() {
     // read images from the given path and return a list
@@ -7,11 +10,18 @@ int main() {
     // for every image in images
     for(const auto& image : images)
     {
-        // create a copy of the original image
-        // convert image into RGB
         // apply Gaussian Blur on image
+        cv::Mat blur_image {};
+        cv::GaussianBlur(image, blur_image, cv::Size(17, 17), 0);
+
         // apply canny on image
+        cv::Mat canny_image {};
+        cv::Canny(blur_image, canny_image, 50, 80);
+
         // apply Hough transform
+        std::vector<cv::Vec2f> lines;
+        cv::HoughLines(canny_image, lines, 2, CV_PI/180, 1, 0, 0 );
+
         // apply Hough line detection, get line candidates
         // get final lines from the candidates
         // filter the slope
@@ -20,6 +30,8 @@ int main() {
         // create a copy of the original image
         // visualize the final lines on the copy image
         // save the visualized image in the output path
+
+        display(canny_image);
     }
 
     return 0;
