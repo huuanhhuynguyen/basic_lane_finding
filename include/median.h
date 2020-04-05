6a74975c72ec86f9a7f7ba58087240e1fa3ca01a
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-05 15:04:27 +0200

    Make function median more generic (accept any STL container).

--- include/median.h
+++ include/median.h
@@ -4,8 +4,9 @@
 #include <vector>
 #include "line.h"
 
-template <typename T>
-T _median(std::vector<T> nums)
+template <typename T,
+          template <typename ELEM, typename = std::allocator<ELEM>> class CONTAINER>
+T median(CONTAINER<T> nums)
 {
     std::sort(nums.begin(), nums.end());
     int mid{nums.size()/2};
@@ -22,8 +23,8 @@ Line get_median_line(const std::vector<Line>& lines)
                    [](const auto& line){ return line.bias(); });
 
     // get the median slope and bias
-    const auto median_slope = _median(slopes);
-    const auto median_bias = _median(biases);
+    const auto median_slope = median(slopes);
+    const auto median_bias = median(biases);
 
     return Line{median_slope, median_bias};
 }
