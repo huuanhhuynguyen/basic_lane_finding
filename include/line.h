791ddfedcade5c362192ffbddf73fd4a7d1e781f
huuanhhuynguyen <huuanhhuynguyen@gmail.com>
2020-04-05 14:34:06 +0200

    Add Line constructor with slope and bias args.

--- include/line.h
+++ include/line.h
@@ -20,6 +20,14 @@ struct Line {
     explicit Line(int _x1, int _y1, int _x2, int _y2) :
             x1{_x1}, y1{_y1}, x2{_x2}, y2{_y2}
     {}
+
+    explicit Line(float slope, float bias)
+    {
+        x1 = 0;
+        y1 = static_cast<int>(slope * x1 + bias);
+        x2 = 10;
+        y2 = static_cast<int>(slope * x2 + bias);
+    }
 private:
     static constexpr float _EPS = 0.000001;  // prevents zero division
 };
